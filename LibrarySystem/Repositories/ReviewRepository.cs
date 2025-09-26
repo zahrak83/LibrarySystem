c53499f5ab7f695050962bb1346cf286d7e03fe2
Zahra <zaahrra8305@gmail.com>
2025-09-26 13:32:56 +0330

    add class reviewService

--- LibrarySystem/Repositories/ReviewRepository.cs
+++ LibrarySystem/Repositories/ReviewRepository.cs
@@ -1,71 +1,78 @@
-﻿//using LibrarySystem.Entities;
-//using LibrarySystem.Infrastructure;
-//using LibrarySystem.Interface;
-//using Microsoft.EntityFrameworkCore;
+﻿using LibrarySystem.Entities;
+using LibrarySystem.Infrastructure;
+using LibrarySystem.Interface;
+using Microsoft.EntityFrameworkCore;
 
-//namespace LibrarySystem.Repositories
-//{
-//    public class ReviewRepository : IReviewRepository
-//    {
-//        AppDBContext _context = new AppDBContext();
+namespace LibrarySystem.Repositories
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        AppDBContext _context = new AppDBContext();
 
-//        public int Create(Review review)
-//        {
-//            review.CreatedAt = DateTime.Now;
-//            //_context.Reviews.Add(review);
-//            _context.SaveChanges();
-//            return review.Id;
-//        }
-//        //public void Delete(int id)
-//        //{
-//        //    var review = _context.Reviews.Find(id);
-//        //    if (review != null)
-//        //    {
-//        //        _context.Reviews.Remove(review);
-//        //        _context.SaveChanges();
-//        //    }
-//        }
-//        public List<Review> GetAll()
-//        {
-//            return _context.Reviews
-//                .Include(r => r.User)
-//                .Include(r => r.Book)
-//                .ThenInclude(b => b.Category)
-//                .ToList();
-//        }
-//        public Review? GetById(int id)
-//        {
-//            return _context.Reviews
-//                .Include(r => r.User)
-//                .Include(r => r.Book)
-//                .ThenInclude(b => b.Category)
-//                .FirstOrDefault(r => r.Id == id);
-//        }
-//        public List<Review> GetByBookId(int bookId)
-//        {
-//            return _context.Reviews
-//                .Include(r => r.User)
-//                .Where(r => r.BookId == bookId && r.IsApproved)
-//                .ToList();
-//        }
-//        public List<Review> GetByUserId(int userId)
-//        {
-//            return _context.Reviews
-//                .Include(r => r.Book)
-//                .ThenInclude(b => b.Category)
-//                .Where(r => r.UserId == userId)
-//                .ToList();
-//        }
-//        public void Update(Review review)
-//        {
-//            var r = _context.Reviews.Find(review.Id);
-//            if (r != null)
-//            {
-//                r.Comment = review.Comment;
-//                r.Rating = review.Rating;
-//                r.IsApproved = review.IsApproved;
-//                _context.SaveChanges();
-//            }
-//        }
-//    }
-//}
+        public int Create(Review review)
+        {
+            review.CreatedAt = DateTime.Now;
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+            return review.Id;
+        }
+
+        public void Delete(int id)
+        {
+            var review = _context.Reviews.Find(id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<Review> GetAll()
+        {
+            return _context.Reviews
+                .Include(r => r.User)
+                .Include(r => r.Book)
+                .ThenInclude(b => b.Category)
+                .ToList();
+        }
+
+        public Review GetById(int id)
+        {
+            return _context.Reviews
+                .Include(r => r.User)
+                .Include(r => r.Book)
+                .ThenInclude(b => b.Category)
+                .FirstOrDefault(r => r.Id == id);
+        }
+
+        public List<Review> GetByBookId(int bookId)
+        {
+            return _context.Reviews
+                .Include(r => r.User)
+                .Where(r => r.BookId == bookId && r.IsApproved)
+                .ToList();
+        }
+
+        public List<Review> GetByUserId(int userId)
+        {
+            return _context.Reviews
+                .Include(r => r.Book)
+                .ThenInclude(b => b.Category)
+                .Where(r => r.UserId == userId)
+                .ToList();
+        }
+
+        public void Update(Review review)
+        {
+            var r = _context.Reviews.Find(review.Id);
+            if (r != null)
+            {
+                r.Comment = review.Comment;
+                r.Rating = review.Rating;
+                r.IsApproved = review.IsApproved;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
+
---
+++ LibrarySystem/Services/ReviewService.cs
@@ -0,0 +1,74 @@
+﻿using LibrarySystem.Entities;
+using LibrarySystem.Interface;
+using LibrarySystem.Interface.Service;
+using LibrarySystem.Repositories;
+
+namespace LibrarySystem.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly ReviewRepository _reviewRepository = new ReviewRepository();
+
+        public int AddReview(int userId, int bookId, string? comment, int rating)
+        {
+            if (rating < 1 || rating > 5)
+                throw new Exception("Rating must be between 1 and 5.");
+
+            var review = new Review
+            {
+                UserId = userId,
+                BookId = bookId,
+                Comment = comment,
+                Rating = rating,
+                CreatedAt = DateTime.Now
+            };
+
+            return _reviewRepository.Create(review);
+        }
+
+        public void ApproveReview(int reviewId)
+        {
+            var review = _reviewRepository.GetById(reviewId);
+            if (review == null)
+                throw new Exception("Review not found.");
+
+            review.IsApproved = true;
+            _reviewRepository.Update(review);
+        }
+
+        public void DeleteReview(int reviewId)
+        {
+            _reviewRepository.Delete(reviewId);
+        }
+
+        public void EditReview(int reviewId, string? comment, int rating)
+        {
+            if (rating < 1 || rating > 5)
+                throw new Exception("Rating must be between 1 and 5.");
+
+            var review = _reviewRepository.GetById(reviewId);
+            if (review == null)
+                throw new Exception("Review not found.");
+
+            review.Comment = comment;
+            review.Rating = rating;
+            _reviewRepository.Update(review);
+        }
+
+        public List<Review> GetAllReviews()
+        {
+            return _reviewRepository.GetAll();
+        }
+
+        public List<Review> GetReviewsByBook(int bookId)
+        {
+            return _reviewRepository.GetByBookId(bookId);
+        }
+
+        public List<Review> GetReviewsByUser(int userId)
+        {
+            return _reviewRepository.GetByUserId(userId);
+        }
+    }
+}
+
