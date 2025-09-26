8eab25f7292a7e3298e981c328e755219871388f
Zahra <zaahrra8305@gmail.com>
2025-09-26 13:28:48 +0330

    add class iReviewService

---
+++ LibrarySystem/Interface/Service/IReviewService.cs
@@ -0,0 +1,15 @@
+﻿using LibrarySystem.Entities;
+
+namespace LibrarySystem.Interface.Service
+{
+    public interface IReviewService
+    {
+        int AddReview(int userId, int bookId, string? comment, int rating);
+        void EditReview(int reviewId, string? comment, int rating);
+        void DeleteReview(int reviewId);
+        void ApproveReview(int reviewId); // فقط Admin
+        List<Review> GetReviewsByBook(int bookId);
+        List<Review> GetReviewsByUser(int userId);
+        List<Review> GetAllReviews(); // برای Admin
+    }
+}
